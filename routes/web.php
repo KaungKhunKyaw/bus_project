a3c5cf9aeacde2b5a30109aa1b8b2d94f0ad0206
KaungKhunKyaw <kaungkhunkyaw86@gmail.com>
2024-07-26 15:54:05 +0630

    Initial Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BusController;
+use App\Http\Controllers\RouteController;
+use App\Http\Controllers\StopController;
+
+Route::get('/buses', [BusController::class, 'index'])->name('buses.index');
+Route::get('/buses/{bus}', [BusController::class, 'show'])->name('buses.show');
+
+Route::get('/buses/{bus}/routes', [RouteController::class, 'index'])->name('routes.index');
+Route::get('/routes/{route}', [RouteController::class, 'show'])->name('routes.show');
+
+Route::get('/routes/{route}/stops', [StopController::class, 'index'])->name('stops.index');
+
 
 Route::get('/', function () {
     return view('welcome');
