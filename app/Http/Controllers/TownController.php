36c662fc129f5090e0a27b8abc1330e8ae204026
KaungKhunKyaw <kaungkhunkyaw86@gmail.com>
2024-07-26 17:17:11 +0630

    Initial TownController

--- app/Http/Controllers/TownController.php
+++ app/Http/Controllers/TownController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Town;
+use Psy\CodeCleaner\FunctionReturnInWriteContextPass;
 
 class TownController extends Controller
 {
-    //
+    public function index()
+    {
+        $towns = Town::all();
+        return view('towns.index', compact('towns'));
+    }
+
+    public function show(Town $town)
+    {
+        return view('towns.show', compact('town'));
+    }  
 }
